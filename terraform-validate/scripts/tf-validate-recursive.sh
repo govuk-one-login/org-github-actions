ae06a0d5fbba2bc85c791878ed169b055447727c
Francis Lewis <francis.lewis@digital.cabinet-office.gov.uk>
2026-05-08 14:43:28 +0100

    feat(actions): Allow terraform validate to work across multiple configurations

---
+++ terraform-validate/scripts/tf-validate-recursive.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT_DIR="$1"
+export TF_PLUGIN_CACHE_DIR="${HOME}/tmp/terraform-plugin-cache"
+mkdir -p "${TF_PLUGIN_CACHE_DIR}"
+
+dirs=$(find "${ROOT_DIR}" -type f -name '*.tf' -not -path '*/.terraform/*' -not -path '*/modules/*' \
+  | xargs -n1 dirname \
+  | sort -u)
+
+if [ -z "${dirs}" ]; then
+  echo "No Terraform directories found"
+  exit 0
+fi
+
+echo "${dirs}" | while read -r dir; do
+  echo -e "\nValidating ${dir}\n"
+  (
+    cd "${dir}"
+    echo "Initalising Terraform"
+    terraform init -backend=false
+    echo "Validating Terraform"
+    terraform validate
+  )
+done
